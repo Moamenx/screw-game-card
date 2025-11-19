28a6ed2ecd2804a7ea800183407a445955ac1716
Moamen <moamen_mohammed@hotmail.com>
2025-11-19 23:56:53 +0300

    Add progressive web apps support

--- ScrewGameCard.Portal/src/app/app.config.ts
+++ ScrewGameCard.Portal/src/app/app.config.ts
@@ -1,13 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-    // Add other providers as needed
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
 };
+
